68a399c588ae37348661e5b54e80888d0f6414eb
AlphaAOmega <france998811@gmail.com>
2021-05-23 01:27:36 +0900

    [EditController] Add Sample CollectionView

--- HabbitManagement/Controller/EditController.swift
+++ HabbitManagement/Controller/EditController.swift
@@ -10,7 +10,7 @@ import UIKit
 class EditController: UIViewController {
     
     // MARK: - Properties
-    
+    var habbitCollectionView: UICollectionView?
     
     // MARK: - Lifecycle
     
@@ -18,6 +18,7 @@ class EditController: UIViewController {
         super.viewDidLoad()
         
         configureUI()
+        habbitCollectionView?.dataSource = self
     }
     
     // MARK: - Actions
@@ -28,11 +29,42 @@ class EditController: UIViewController {
         present(nav, animated: true, completion: nil)
     }
     
-    // MARK: - Helpers
+    // MARK: - Methods
     
     func configureUI() {
-        view.backgroundColor = .systemBlue
-        
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus_unselected"), style: .plain, target: self, action: #selector(TapAdd))
+        
+        addCollectionView()
     }
+    
+    func addCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        
+        let size = (view.frame.width - 40) / 2
+        
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 20, right: 10)
+        layout.itemSize = CGSize(width: size, height: size)
+        
+        habbitCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        
+        habbitCollectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "habbitCell")
+        habbitCollectionView?.backgroundColor = .blue
+        
+        view.addSubview(habbitCollectionView ?? UICollectionView())
+        habbitCollectionView?.reloadData()
+    }
+}
+
+extension EditController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = habbitCollectionView?.dequeueReusableCell(withReuseIdentifier: "habbitCell", for: indexPath)
+        cell?.backgroundColor = .black
+        return cell ?? UICollectionViewCell()
+    }
+    
+    
 }
